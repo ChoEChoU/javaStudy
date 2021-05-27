841b4502a731b1ada6b4562bab53275c66db8ab1
ChoEChoU <44629655+ChoEChoU@users.noreply.github.com>
2021-05-27 11:52:44 +0900

    Create base
    Create Class and write base code

---
+++ javaStudy1/src/javaStudy1/Queue.java
@@ -0,0 +1,35 @@
+package javaStudy1;
+
+import java.util.ArrayList;
+
+public class Queue {
+	ArrayList<Integer> queue = new ArrayList<Integer>();
+	
+	public void push(Integer X) {
+		queue.add(X);
+	}
+	
+	public Integer pop() {
+		return 1;
+	}
+	
+	public Integer size() {
+		return 1;
+	}
+	
+	public Integer empty() {
+		 return 1;
+	}
+	
+	public Integer front() {
+		return 1;
+	}
+	
+	public Integer back() {
+		return 1;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+ }
