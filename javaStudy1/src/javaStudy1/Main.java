1fd54a44b04c7a30c336ce8ceb6a79667ae4f1fc
ChoEChoU <44629655+ChoEChoU@users.noreply.github.com>
2021-05-29 14:40:54 +0900

    Modify main code

--- javaStudy1/src/javaStudy1/Main.java
+++ javaStudy1/src/javaStudy1/Main.java
@@ -9,47 +9,67 @@ public class Main {
 		
 		Scanner input = new Scanner(System.in);
 		
-		Integer number = input.nextInt();
+		Integer lineNumber = input.nextInt();
 		
-		for (int i = 0; i < number; i++) {
-			String command = input.nextLine();
-			
-		}
-		System.out.println("Queue 출력값");
 		Queue queue = new Queue();
-		queue.push(1);
-		queue.push(2);
-		System.out.println(queue.front());
-		System.out.println(queue.back());
-		System.out.println(queue.size());
-		System.out.println(queue.empty());
-		System.out.println(queue.pop());
-		System.out.println(queue.pop());
-		System.out.println(queue.pop());
-		System.out.println(queue.size());
-		System.out.println(queue.empty());
-		System.out.println(queue.pop());
-		queue.push(3);
-		System.out.println(queue.empty());
-		System.out.println(queue.front());
 		
-		System.out.print("\n");
-		
-		System.out.println("Stack 출력값");
-		Stack stack = new Stack();
-		stack.push(1);
-		stack.push(2);
-		System.out.println(stack.top());
-		System.out.println(stack.size());
-		System.out.println(stack.empty());
-		System.out.println(stack.pop());
-		System.out.println(stack.pop());
-		System.out.println(stack.pop());
-		System.out.println(stack.size());
-		System.out.println(stack.empty());
-		System.out.println(stack.pop());
-		stack.push(3);
-		System.out.println(stack.empty());
-		System.out.println(stack.top());
+		for (int i = 0; i < lineNumber; i++) {
+			String command = input.nextLine();
+			switch (command) {
+			case "push":
+				Integer pushNumber = input.nextInt();
+				queue.push(pushNumber);
+
+			case "pop":
+				System.out.println(queue.pop());
+				
+			case "size":
+				System.out.println(queue.size());
+				
+			case "empty":
+				System.out.println(queue.empty());
+				
+			case "front":
+				System.out.println(queue.front());
+				
+			case "back":
+				System.out.println(queue.back());
+			}
+		input.close();
+//		queue.push(1);
+//		queue.push(2);
+//		System.out.println(queue.front());
+//		System.out.println(queue.back());
+//		System.out.println(queue.size());
+//		System.out.println(queue.empty());
+//		System.out.println(queue.pop());
+//		System.out.println(queue.pop());
+//		System.out.println(queue.pop());
+//		System.out.println(queue.size());
+//		System.out.println(queue.empty());
+//		System.out.println(queue.pop());
+//		queue.push(3);
+//		System.out.println(queue.empty());
+//		System.out.println(queue.front());
+//		
+//		System.out.print("\n");
+//		
+//		System.out.println("Stack 출력값");
+//		Stack stack = new Stack();
+//		stack.push(1);
+//		stack.push(2);
+//		System.out.println(stack.top());
+//		System.out.println(stack.size());
+//		System.out.println(stack.empty());
+//		System.out.println(stack.pop());
+//		System.out.println(stack.pop());
+//		System.out.println(stack.pop());
+//		System.out.println(stack.size());
+//		System.out.println(stack.empty());
+//		System.out.println(stack.pop());
+//		stack.push(3);
+//		System.out.println(stack.empty());
+//		System.out.println(stack.top());
+		}
 	}
 }
