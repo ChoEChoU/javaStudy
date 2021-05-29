d35bd9a7c984758203d374515807e48242a1f230
ChoEChoU <44629655+ChoEChoU@users.noreply.github.com>
2021-05-29 15:20:54 +0900

    Add stack command code

--- javaStudy1/src/javaStudy1/Main.java
+++ javaStudy1/src/javaStudy1/Main.java
@@ -12,6 +12,7 @@ public class Main {
 		Integer lineNumber = input.nextInt();
 		
 		Queue queue = new Queue();
+		//Stack stack = new Stack();
 		
 		for (int i = 0; i < lineNumber; i++) {
 			String command = input.next();
@@ -19,18 +20,22 @@ public class Main {
 			case "push":
 				Integer pushNumber = input.nextInt();
 				queue.push(pushNumber);
+				//stack.push(pushNumber);
 				break;
 
 			case "pop":
 				System.out.println(queue.pop());
+//				System.out.println(stack.pop());
 				break;
 				
 			case "size":
 				System.out.println(queue.size());
+//				System.out.println(stack.size());
 				break;
 				
 			case "empty":
 				System.out.println(queue.empty());
+//				System.out.println(stack.empty());
 				break;
 				
 			case "front":
@@ -40,42 +45,12 @@ public class Main {
 			case "back":
 				System.out.println(queue.back());
 				break;
+				
+//			case "top":
+//				System.out.println(stack.top());
+//				break;
 			}
 		}
 		input.close();
-//		queue.push(1);
-//		queue.push(2);
-//		System.out.println(queue.front());
-//		System.out.println(queue.back());
-//		System.out.println(queue.size());
-//		System.out.println(queue.empty());
-//		System.out.println(queue.pop());
-//		System.out.println(queue.pop());
-//		System.out.println(queue.pop());
-//		System.out.println(queue.size());
-//		System.out.println(queue.empty());
-//		System.out.println(queue.pop());
-//		queue.push(3);
-//		System.out.println(queue.empty());
-//		System.out.println(queue.front());
-//		
-//		System.out.print("\n");
-//		
-//		System.out.println("Stack Ãâ·Â°ª");
-//		Stack stack = new Stack();
-//		stack.push(1);
-//		stack.push(2);
-//		System.out.println(stack.top());
-//		System.out.println(stack.size());
-//		System.out.println(stack.empty());
-//		System.out.println(stack.pop());
-//		System.out.println(stack.pop());
-//		System.out.println(stack.pop());
-//		System.out.println(stack.size());
-//		System.out.println(stack.empty());
-//		System.out.println(stack.pop());
-//		stack.push(3);
-//		System.out.println(stack.empty());
-//		System.out.println(stack.top());
 	}
 }
