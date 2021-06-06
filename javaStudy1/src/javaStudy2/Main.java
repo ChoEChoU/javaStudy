aab66fef651f6800557e60eb0bf9baf69fc76b34
ChoEChoU <44629655+ChoEChoU@users.noreply.github.com>
2021-06-06 13:36:20 +0900

    Write Main class code

--- javaStudy1/src/javaStudy2/Main.java
+++ javaStudy1/src/javaStudy2/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] array = {1, 5, 4, 3, 2};
+		System.out.println(array[2]);
+		
+		BubbleSort sort = new BubbleSort();
+		sort.sort(array);
+		System.out.println(array[2]);
 	}
 
 }
