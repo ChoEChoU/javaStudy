4807b8c201e710e488541ce8db2597da15e6d6e1
ChoEChoU <44629655+ChoEChoU@users.noreply.github.com>
2021-06-06 13:29:40 +0900

    Write BubbleSort Code

--- javaStudy1/src/javaStudy2/BubbleSort.java
+++ javaStudy1/src/javaStudy2/BubbleSort.java
@@ -1,5 +1,25 @@
 package javaStudy2;
 
 public class BubbleSort {
-
+	private int swapCnt;
+	public BubbleSort() {
+	}
+	
+	public int[] sort(int[] inputArray){
+		for (int i = 0; i < inputArray.length; i++) {
+			for (int j = 0; j < inputArray.length-i; j++) {
+				if (inputArray[i] < inputArray[i+1]) {
+					int temp = inputArray[i];
+					inputArray[i] = inputArray[i+1];
+					inputArray[i+1] = temp;
+					swapCnt++;
+				}
+			}
+		}
+		return inputArray;
+	}
+	
+	public int getSwapCnt() {
+		return swapCnt;
+	}
 }
