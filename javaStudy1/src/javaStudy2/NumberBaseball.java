8f6663c725ad7cfd6215be899d6fe8f726382bd4
ChoEChoU <44629655+ChoEChoU@users.noreply.github.com>
2021-06-12 20:15:56 +0900

    Add makeNumber function and write code

--- javaStudy1/src/javaStudy2/NumberBaseball.java
+++ javaStudy1/src/javaStudy2/NumberBaseball.java
@@ -1,5 +1,29 @@
 package javaStudy2;
 
-public class NumberBaseball {
+import java.util.ArrayList;
+import java.util.Random;
 
+public class NumberBaseball {
+	private ArrayList<Integer> numberSet;
+	
+	public void makeNumber() {
+		Random random = new Random();
+		int num1 = random.nextInt(10);
+		
+		int num2 = random.nextInt(10);
+		
+		int num3 = random.nextInt(10);
+		
+		while (num2 == num1) {
+			num2 = random.nextInt(10);
+		}
+		
+		while (num3 == num2 || num3 == num1) {
+			num2 = random.nextInt(10);
+		}
+		
+		numberSet.add(num1);
+		numberSet.add(num2);
+		numberSet.add(num3);
+	}
 }
