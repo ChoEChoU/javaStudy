7de2449d2731be47f43b372f87d558a14d5661d7
ChoEChoU <44629655+ChoEChoU@users.noreply.github.com>
2021-06-20 14:46:22 +0900

    Modify main class code

--- javaStudy1/src/javaStudy2/NumberBaseball.java
+++ javaStudy1/src/javaStudy2/NumberBaseball.java
@@ -61,6 +61,30 @@ public class NumberBaseball {
 	}
 	
 	public int predictNumber(ArrayList<ArrayList<Integer>> numSet) {
+		ArrayList<Integer> numList = new ArrayList<Integer>();
+		
+		for (int i = 0; i < 10; i++) {
+			numList.add(i);
+		}
+		
+		for (int i = 0; i < numSet.size(); i++) {
+			int number = numSet.get(i).get(0);
+			
+			String strNumber = String.valueOf(number);
+			
+			int strike = numSet.get(i).get(1);
+			int ball = numSet.get(i).get(2);
+			
+			if (strike == 0 && ball == 0) {
+				numList.remove(strNumber.indexOf(0));
+				numList.remove(strNumber.indexOf(1));
+				numList.remove(strNumber.indexOf(2));
+			}
+			
+			if (strike + ball == 3) {
+				numList.remove(strNumber.indexOf(0));
+			}
+		}
 		return 0;
 	}
 }
