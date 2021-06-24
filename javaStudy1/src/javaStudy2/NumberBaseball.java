26329e35f61a1f342e6782092db768e4508bf71e
ChoEChoU <44629655+ChoEChoU@users.noreply.github.com>
2021-06-24 18:38:52 +0900

    Modify predictNumber method in NumberBaseball class

--- javaStudy1/src/javaStudy2/NumberBaseball.java
+++ javaStudy1/src/javaStudy2/NumberBaseball.java
@@ -6,26 +6,26 @@ import java.util.Random;
 public class NumberBaseball {
 	private ArrayList<Integer> numberSet = new ArrayList<Integer>();
 	
-	public void makeNumber() {
-		Random random = new Random();
-		int num1 = random.nextInt(10);
-		
-		int num2 = random.nextInt(10);
-		
-		int num3 = random.nextInt(10);
-		
-		while (num2 == num1) {
-			num2 = random.nextInt(10);
-		}
-		
-		while (num3 == num2 || num3 == num1) {
-			num3 = random.nextInt(10);
-		}
-		
-		numberSet.add(num1);
-		numberSet.add(num2);
-		numberSet.add(num3);
-	}
+//	public void makeNumber() {
+//		Random random = new Random();
+//		int num1 = random.nextInt(10);
+//		
+//		int num2 = random.nextInt(10);
+//		
+//		int num3 = random.nextInt(10);
+//		
+//		while (num2 == num1) {
+//			num2 = random.nextInt(10);
+//		}
+//		
+//		while (num3 == num2 || num3 == num1) {
+//			num3 = random.nextInt(10);
+//		}
+//		
+//		numberSet.add(num1);
+//		numberSet.add(num2);
+//		numberSet.add(num3);
+//	}
 	
 	public ArrayList<Integer> playGame(int num1, int num2, int num3) {
 		int strike = 0;
@@ -53,10 +53,6 @@ public class NumberBaseball {
 			ball++;
 		}
 		
-		if (!numberSet.contains(num1) && !numberSet.contains(num2) && !numberSet.contains(num3)) {
-			
-		}
-		
 		ArrayList<Integer> strikeBall = new ArrayList<Integer>();
 		strikeBall.add(strike);
 		strikeBall.add(ball);
@@ -68,28 +64,34 @@ public class NumberBaseball {
 		
 		for (int i = 123; i < 1000; i++) {
 			int firstI = i / 100;
-			int secondI = i % 100;
+			int secondI = i % 100 / 10;
 			int thirdI = i % 100 % 10;
 			
 			if (firstI == secondI && secondI == thirdI && thirdI == firstI) {
 				continue;
 			}
 			
+			numberSet.add(firstI);
+			numberSet.add(secondI);
+			numberSet.add(thirdI);
+			
+			boolean flag = true;
+			
 			for (int j = 0; j < numSet.size(); j++) {
 				int number = numSet.get(j).get(0);
-				
-				numberSet.add(number/100);
-				numberSet.add(number%100/10);
-				numberSet.add(number%100%10);
-				
 				int strike = numSet.get(j).get(1);
 				int ball = numSet.get(j).get(2);
 				
-				ArrayList<Integer> gameResult = playGame(firstI, secondI, thirdI);
+				ArrayList<Integer> gameResult = playGame(number / 100, number % 100 / 10, number % 100 % 10);
 				if (gameResult.get(0) == strike && gameResult.get(1) == ball) {
-					result.add(i);
+					continue;
 				}
-				
+				else {
+					flag = false;
+				}	
+			}
+			if (flag == true) {
+				result.add(i);
 			}
 			numberSet.clear();
 		}
