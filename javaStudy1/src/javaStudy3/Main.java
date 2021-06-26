68d21bc8ae48b482b17a319d50d4127d3a5d05cc
ChoEChoU <44629655+ChoEChoU@users.noreply.github.com>
2021-06-26 12:01:52 +0900

    Modify Main class code

--- javaStudy1/src/javaStudy3/Main.java
+++ javaStudy1/src/javaStudy3/Main.java
@@ -1,11 +1,25 @@
 package javaStudy3;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Factorial factorial = new Factorial();
-		System.out.println(factorial.calculation(10));
+//		Factorial factorial = new Factorial();
+//		System.out.println(factorial.calculation(4));
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int m = scan.nextInt();
+		
+		int[] numSet = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			int number = scan.nextInt();
+			numSet[i] = number;
+		}
 	}
 
 }
