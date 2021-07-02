51164c0778c7b6cea29fad98c388f14dd98ca4df
ChoEChoU <44629655+ChoEChoU@users.noreply.github.com>
2021-07-02 19:45:25 +0900

    Modify Main class code

--- javaStudy1/src/javaStudy4/Main.java
+++ javaStudy1/src/javaStudy4/Main.java
@@ -1,33 +1,24 @@
 package javaStudy4;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Node node1 = new Node(50);
-		Node node2 = new Node(30);
-		Node node3 = new Node(24);
-		Node node4 = new Node(5);
-		Node node5 = new Node(28);
-		Node node6 = new Node(45);
-		Node node7 = new Node(98);
-		Node node8 = new Node(52);
-		Node node9 = new Node(60);
+		
+		Scanner scan = new Scanner(System.in);
 		
 		BinarySearchTree tree = new BinarySearchTree();
 		
-		tree.insert(node1);
-		tree.insert(node2);
-		tree.insert(node3);
-		tree.insert(node4);
-		tree.insert(node5);
-		tree.insert(node6);
-		tree.insert(node7);
-		tree.insert(node8);
-		tree.insert(node9);
+		for (int i = 0; i < 9; i++) {
+			Node node = new Node(scan.nextInt());
+			tree.insert(node);	
+		}
 		
-		tree.postOrder(node1);
+		tree.postOrder(tree.getRoot());
 		
+		scan.close();
 	}
 
 }
