f55fad14a7c8c16fc4509ab8f500cf747e0e3df8
ChoEChoU <44629655+ChoEChoU@users.noreply.github.com>
2021-07-19 19:42:15 +0900

    Modify BusRoute class code

--- javaStudy1/src/javaStudy5/BusRoute.java
+++ javaStudy1/src/javaStudy5/BusRoute.java
@@ -1,14 +1,27 @@
 package javaStudy5;
 
+import java.util.ArrayList;
+
 public class BusRoute {
 	private BusStation station;
 	private int first;
 	private int second;
+	private ArrayList<BusStation> set;
 	
-	public BusRoute(int first, int second) {
+	public BusRoute(int first, int second, ArrayList<BusStation> stationSet) {
 		this.first = first;
 		this.second = second;
+		set = stationSet;
 	}
 	
-	
+	public void routeNumber() {
+		if (first < second) {
+			while (first < second) {
+				int number = set.get(set.indexOf(first)).getStationNum();
+			}
+		} 
+		else if (second < first){
+
+		}
+	}
 }
